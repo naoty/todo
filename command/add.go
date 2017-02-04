a4b1a938ab8624a1804c2b75be80e29f2aea85bd
Naoto Kaneko <naoty.k@gmail.com>
2017-02-04 13:35:08 +0900

    Add a todo to a subtodo

--- command/add.go
+++ command/add.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/naoty/todo/todo"
@@ -13,7 +14,7 @@ var Add = cli.Command{
 	Usage:  "Add a todo",
 	Action: add,
 	Flags: []cli.Flag{
-		cli.IntFlag{
+		cli.StringFlag{
 			Name:  "parent, p",
 			Usage: "Specify the parent todo",
 		},
@@ -35,14 +36,15 @@ func add(c *cli.Context) error {
 	title := strings.Join(c.Args(), " ")
 	todo := todo.Todo{Title: title, Done: false, Todos: []todo.Todo{}}
 
-	p := c.Int("parent")
-	if p == 0 {
-		todos = append(todos, todo)
-	} else if len(todos) > p-1 {
-		parent := todos[p-1]
-		parent.Todos = append(parent.Todos, todo)
-		todos[p-1] = parent
+	var orders []int
+	parent := c.String("parent")
+	for _, id := range strings.Split(parent, "-") {
+		if order, err2 := strconv.Atoi(id); err2 == nil {
+			orders = append(orders, order)
+		}
 	}
+
+	todos = addTodo(todo, todos, orders)
 
 	err = writeTodos(todos, path)
 	if err != nil {
@@ -50,4 +52,26 @@ func add(c *cli.Context) error {
 	}
 
 	return nil
+}
+
+func addTodo(todo todo.Todo, todos []todo.Todo, orders []int) []todo.Todo {
+	if len(orders) == 0 {
+		todos = append(todos, todo)
+		return todos
+	}
+
+	i := orders[0] - 1
+	if i >= len(todos) {
+		return todos
+	}
+
+	t := todos[i]
+	if len(orders) == 1 {
+		t.Todos = append(todo.Todos, todo)
+	} else {
+		t.Todos = addTodo(todo, t.Todos, orders[1:])
+	}
+	todos[i] = t
+
+	return todos
 }
