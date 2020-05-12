60c2b3bc10556faecfb9b0e02ce03d883d8d1784
Naoto Kaneko <naoty.k@gmail.com>
2020-05-12 23:11:24 +0900

    Archive all done TODOs when ID isn't passed

--- cmd/archive.go
+++ cmd/archive.go
@@ -21,11 +21,14 @@ func NewArchive(cli CLI, version string, repo repository.Repository) Command {
 
 // Run implements Command interface.
 func (c *Archive) Run(args []string) int {
-	if len(args) < 3 {
-		fmt.Fprintf(c.cli.ErrorWriter, usage())
-		return 1
+	if len(args) == 2 {
+		return c.archiveTodos(args)
 	}
 
+	return c.archiveTodo(args)
+}
+
+func (c *Archive) archiveTodo(args []string) int {
 	id, err := strconv.Atoi(args[2])
 	if err != nil {
 		fmt.Fprintln(c.cli.ErrorWriter, usage())
@@ -38,12 +41,48 @@ func (c *Archive) Run(args []string) int {
 		return 1
 	}
 
-	td.State = todo.Archived
-	err = c.repo.Update(td)
+	err = c.archive(td)
 	if err != nil {
 		fmt.Fprintln(c.cli.ErrorWriter, usage())
 		return 1
 	}
 
 	return 0
+}
+
+func (c *Archive) archiveTodos(args []string) int {
+	todos, err := c.repo.List()
+	if err != nil {
+		fmt.Fprintln(c.cli.ErrorWriter, err)
+		return 1
+	}
+
+	for _, td := range todos {
+		err := c.archive(td)
+		if err != nil {
+			fmt.Fprintln(c.cli.ErrorWriter, err)
+			return 1
+		}
+	}
+
+	return 0
+}
+
+func (c *Archive) archive(td *todo.Todo) error {
+	if td.State == todo.Done {
+		td.State = todo.Archived
+		err := c.repo.Update(td)
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, sub := range td.Todos {
+		err := c.archive(sub)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
